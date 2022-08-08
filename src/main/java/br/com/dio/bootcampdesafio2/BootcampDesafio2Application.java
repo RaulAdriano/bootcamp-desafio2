4a4f9689ff58f02d93005d86c85ffb8a92a80151
Raul Adriano Ramos - PrestServ <raul.adriano.prestserv@petrobras.com.br>
2022-08-08 16:20:49 -0300

    Initial commit

---
+++ src/main/java/br/com/dio/bootcampdesafio2/BootcampDesafio2Application.java
@@ -0,0 +1,13 @@
+package br.com.dio.bootcampdesafio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampDesafio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampDesafio2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dio/bootcampdesafio2/BootcampDesafio2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.bootcampdesafio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampDesafio2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
