17ff46eaf3554a8a729b7aa585f0c72e36ad67e2
Raul Adriano Ramos <raul.adriano.ramos@gmail.com>
2022-08-09 13:48:53 -0300

    final

--- src/main/java/br/com/dio/bootcampdesafio2/repository/AlunoRepository.java
+++ src/main/java/br/com/dio/bootcampdesafio2/repository/AlunoRepository.java
@@ -4,6 +4,11 @@ import br.com.dio.bootcampdesafio2.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface AlunoRepository  extends JpaRepository<Aluno, Long> {
+
+    List<Aluno> findByDataDeNascimento(LocalDate dataDeNascimento);
 }
